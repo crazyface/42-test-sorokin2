565620a4e0c414779bbc51d48980ab5c891b8557
Alexand Sorokin <bsorokind@gmail.com>
2012-07-05 03:04:51 +0300

    datepicker bugfix

--- sorokin_test2/static/js/profile_form.js
+++ sorokin_test2/static/js/profile_form.js
@@ -44,6 +44,7 @@ $(document).ready(function(){
 	    		if(data.find('.errorlist').length){
 		    		$('#profile_form').replaceWith(data)
 		    		createUploader();
+		    		$( ".datepicker" ).datepicker({'font-size':'10px', 'dateFormat': 'yy-mm-dd'});
 	    		}else{
 	    			document.location.href = $('#profile_form').attr('success_url')
 	    		}
